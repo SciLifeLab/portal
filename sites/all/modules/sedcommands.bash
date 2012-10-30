b235f3fbdf8b48c2198df90f31593c572e22dc1c
jonas <jonas@gallus.scilifelab.se>
2012-10-30 17:44:48 +0100

    added feature for order with sed commands

---
+++ sites/all/modules/sedcommands.bash
@@ -0,0 +1,40 @@
+sed -i 's/field_bioinformatics_expertise_i/field_bioinf_expertise/' * 
+sed -i 's/field_previous_experience_with_n/field_bioinf_prev_exp_ngs/' * 
+sed -i 's/field_best_practice_basic_analys/field_bioinf_best_practice/' * 
+sed -i 's/field_total_number_of_picotiter_/field_flx_totnum_picotiter/' * 
+sed -i 's/field_regions_per_plate/field_flx_reg_per_plate/' *
+sed -i 's/field_file_attachments/field_geninfo_files/' *
+sed -i 's/field_existing_uppnex_project/field_geninfo_ex_uppnex_proj/' *
+sed -i 's/field_your_project_name/field_geninfo_cust_proj_name/' * 
+sed -i 's/field_instrument/field_illumina_instrument/' *
+sed -i 's/field_hiseq_config/field_illumina_hiseq_conf/' *
+sed -i 's/field_hiseq_config_other/field_illumina_hiseq_conf_oth/' *
+sed -i 's/field_miseq_config/field_illumina_miseq_conf/' *
+sed -i 's/field_miseq_config_other/field_illumina_miseq_conf_oth/' *
+sed -i 's/field_illumintype_of_dna_library/field_illumina_dnalib_type/' *
+sed -i 's/field_iontorrent_read_length/field_ion_tor_rlength/' *
+sed -i 's/field_ionproton_read_length/field_ion_pro_rlength/' *
+sed -i 's/field_barcoding_required/field_ion_barcode_req/' * 
+sed -i 's/field_torrent_throughput_chip/field_ion_tor_thrput_chip/' *
+sed -i 's/field_proton_throughput_chip/field_ion_pro_thrput_chip/' *
+sed -i 's/field_new_project_name/field_new_upnx_proj_name/' *
+sed -i 's/field_tech_admin_name/field_new_upnx_tadmin_name/' *
+sed -i 's/field_tech_admin_email/field_new_upnx_tadmin_email/' *
+sed -i 's/field_other_uppnex_project_membe/field_new_upnx_oth_member/' *
+sed -i 's/field_link_to_reference/field_ordinf_reflink/' *
+sed -i 's/field_genome_size_/field_ordinf_genome_size/' *
+sed -i 's/field_handling_of_leftover_sampl/field_ordinf_left_sample/' *
+sed -i 's/field_handling_of_leftover_libra/field_ordinf_left_lib/' *
+sed -i 's/field_platform/field_ordinf_platform/' *
+sed -i 's/field_number_of_samples/field_ordinf_num_samp/' *
+sed -i 's/field_special_requirements/field_ordinf_special_req/' *
+sed -i 's/field_which_scilifelab_node_shou/field_ordinf_scilife_node/' *
+sed -i 's/field_other_species/field_ordinf_species_oth/' *
+sed -i 's/field_expected_date_of_sample_de/field_ordinf_date_samp_dev/' *
+sed -i 's/field_order_info_supported_speci/field_ordinf_species/' *
+sed -i 's/field_coverage/field_ordinf_coverage/' *
+
+
+
+
+
